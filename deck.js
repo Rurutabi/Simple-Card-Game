5f2f224262f14f7a27b14c4090ead5bc3e571413
Sira <sirahop_2538@hotmail.com>
2022-10-15 10:09:37 +1100

    Setting up Deck of Cards

--- deck.js
+++ deck.js
@@ -3,7 +3,18 @@ const VALUES = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
 
 export default class Deck{
     constructor(cards = freshDeck()){
-        this.card = cards
+        this.cards = cards
+    }
+    get numberOfCards(){
+        return this.cards.length
+    }
+    shuffle(){
+        for(let i = this.numberOfCards -1; i > 0; i--){
+            const newIndex = Math.floor(Math.random() * (i + 1))
+            const oldValue = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oldValue
+        }
     }
 }
 
